a3298a8ba143b3873ef804836491a3ed97abb4c3
JLNY <allenlicolumbia@gmail.com>
2018-02-19 23:03:40 -0500

    commit

--- price_match.py
+++ price_match.py
@@ -2,11 +2,13 @@ import gdax
 import gzip
 import ast
 import csv
-from time import gmtime, strftime
+from time import gmtime, strftime, localtime
+import time
 from websocket import create_connection
 from urllib import request
 import krakenex
 from pykrakenapi import KrakenAPI
+import psycopg2
 
 
 #GDAX price
@@ -37,6 +39,29 @@ def get_price_from_kraken(kraken_token):
     krakenprice = krakenApi.get_ticker_information(kraken_token).loc[kraken_token]['a'][0]
     return float(krakenprice)
 
+#saveDataToDB
+def save_data_to_DB(command):
+    conn = psycopg2.connect("dbname='BitCoinArb' user=postgres password=postgres")
+    # command = """
+    #     CREATE TABLE Price (
+    #         time TIMESTAMP ,
+    #         token VARCHAR(50),
+    #         profit FLOAT
+    #     )
+    #     """
+    cur = conn.cursor()
+    cur.execute(command)
+    cur.close()
+    conn.commit()
+
+def save_price_to_DB(timestamp, token, profit):
+    sqlbase = """
+        INSERT INTO price(datetime, token, profit)
+        VALUES (CURRENT_TIMESTAMP , '%s', %f);
+    """
+    sql = sqlbase % (token, profit)
+    save_data_to_DB(sql)
+
 def get_arbitrage_for_currecies(gdax_token, huobi_token, kraken_token = 'XXBTZUSD'):
     gdax_price = get_price_from_gdax(gdax_token)
     huobi_price = get_price_from_huobi(huobi_token)
@@ -45,18 +70,25 @@ def get_arbitrage_for_currecies(gdax_token, huobi_token, kraken_token = 'XXBTZUS
     return arbitrage_profit
 
 if __name__ == '__main__':
-    with open('E://BitCoin//arbitrage.csv', 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=' ')
-        while(1):
-            arb_btcusd = get_arbitrage_for_currecies('BTC-USD', 'btcusdt', 'XXBTZUSD')
-            arb_ethbtc = get_arbitrage_for_currecies('ETH-BTC', 'ethbtc', 'XETHXXBT')
-            arb_ltcbtc = get_arbitrage_for_currecies('LTC-BTC', 'ltcbtc', 'XLTCXXBT')
-            csvwriter.writerow(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ', BTC-USD, ' + str(arb_btcusd))
-            csvwriter.writerow(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ', ETH-BTC, ' + str(arb_ethbtc))
-            csvwriter.writerow(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ', LTC-BTC, ' + str(arb_ltcbtc))
-            print(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ', BTC-USD, ' + str(arb_btcusd))
-            print(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ', ETH-BTC, ' + str(arb_ethbtc))
-            print(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ', LTC-BTC, ' + str(arb_ltcbtc))
+    while(1):
+        arb_btcusd = get_arbitrage_for_currecies('BTC-USD', 'btcusdt', 'XXBTZUSD')
+        save_price_to_DB(time.time(), 'BTC-USD', arb_btcusd)
+        arb_ethbtc = get_arbitrage_for_currecies('ETH-BTC', 'ethbtc', 'XETHXXBT')
+        save_price_to_DB(time.time(), 'ETH-BTC', arb_ethbtc)
+        arb_ltcbtc = get_arbitrage_for_currecies('LTC-BTC', 'ltcbtc', 'XLTCXXBT')
+        save_price_to_DB(time.time(), 'LTC-BTC', arb_ltcbtc)
+    # with open('E://BitCoin//arbitrage.csv', 'w', newline='') as csvfile:
+    #     csvwriter = csv.writer(csvfile, delimiter=' ')
+    #     while(1):
+    #         arb_btcusd = get_arbitrage_for_currecies('BTC-USD', 'btcusdt', 'XXBTZUSD')
+    #         arb_ethbtc = get_arbitrage_for_currecies('ETH-BTC', 'ethbtc', 'XETHXXBT')
+    #         arb_ltcbtc = get_arbitrage_for_currecies('LTC-BTC', 'ltcbtc', 'XLTCXXBT')
+    #         csvwriter.writerow(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ', BTC-USD, ' + str(arb_btcusd))
+    #         csvwriter.writerow(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ', ETH-BTC, ' + str(arb_ethbtc))
+    #         csvwriter.writerow(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ', LTC-BTC, ' + str(arb_ltcbtc))
+    #         print(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ', BTC-USD, ' + str(arb_btcusd))
+    #         print(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ', ETH-BTC, ' + str(arb_ethbtc))
+    #         print(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ', LTC-BTC, ' + str(arb_ltcbtc))
 
 
 
